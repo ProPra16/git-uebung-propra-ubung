53ec5e32f31d295d9ad4dd3a7cf596a10780e66e
NatalieHHU <natalie.braam@hhu.de>
2016-06-15 13:51:29 +0200

    Button alarm

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,24 @@
 package de.hhu.propra16;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+	
+	public Button Alarm() {
+
+		BorderPane border = new BorderPane();
+		Button Alarm = new Button("Alarm");
+		Alarm.setOnAction(event -> { 
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			alert.setTitle("Fehler");
+			alert.setContentText("Too Many Buttons");
+			alert.showAndWait();
+		});
+	
+	}
 }
