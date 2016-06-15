6efa56952443e274c23bf73e6e336107f9bbc7d7
favu100 <Fabian.Vu@hhu>
2016-06-15 13:52:59 +0200

    Button 2,4

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -4,12 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtons extends Application {
+	
+	static Stage primaryStage;
+	
     public static void main(String[] args) {
         launch(args);
     }
@@ -18,9 +22,15 @@ public class FiveButtons extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/five_buttons" +
                 ".fxml"));
-
+        this.primaryStage = primaryStage;
         primaryStage.setTitle("Five Buttons");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
+    
+    public static void farbe() {
+    	primaryStage.getScene().setFill(Color.BLUE);
+    }
+    
+    
 }
--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,48 @@
 package de.hhu.propra16;
 
+import java.io.File;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+	
+	@FXML
+	Button btlabel;
+	
+	@FXML
+	Label text;
+	
+	@FXML
+	Button btblau;
+	
+	
+	@FXML
+	protected void dateiladen(ActionEvent event) {
+		FileChooser dialog = new FileChooser();
+		File file = dialog.showOpenDialog(new Stage());
+		
+		text.setText(file.getName());
+		
+		
+	}
+	
+	@FXML
+	protected void changecolor(ActionEvent event) {
+		
+		FiveButtons.farbe();
+		
+	}
+	
+	
+	
+	
 }
